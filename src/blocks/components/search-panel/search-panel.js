127d59df8bbd0fe70360a7afe837bae6449685bb
Taras Slovak <slovak.taras@gmail.com>
2022-10-07 23:36:24 +0300

    init and added firt blocks

---
+++ src/blocks/components/search-panel/search-panel.js
@@ -0,0 +1,4 @@
+/* eslint-disable no-unused-vars */
+import datepicker from "js-datepicker";
+
+const picker = datepicker("#data-picker", {});
---
+++ src/js/import/components.js
@@ -0,0 +1,1 @@
+import "%components%/search-panel/search-panel";
