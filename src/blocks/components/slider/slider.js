4c793d06dd08daf6984a9523724f5aff54adbe63
Taras Slovak <slovak.taras@gmail.com>
2022-10-09 23:12:36 +0300

    added code

---
+++ src/blocks/components/slider/slider.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import { Carousel } from "bootstrap";
+
+const myCarouselElement = document.querySelector("#slider-sauna");
+const carousel = new Carousel(myCarouselElement, {
+  wrap: true,
+});
