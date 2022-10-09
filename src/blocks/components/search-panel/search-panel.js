8645613d2548ac5cc8b5b1f82b9a1d66a5abe8f6
Taras Slovak <slovak.taras@gmail.com>
2022-10-09 10:46:35 +0300

    added code

--- src/blocks/components/search-panel/search-panel.js
+++ src/blocks/components/search-panel/search-panel.js
@@ -1,4 +1,20 @@
 /* eslint-disable no-unused-vars */
 import datepicker from "js-datepicker";
 
-const picker = datepicker("#data-picker", {});
+const picker = datepicker("#data-picker", {
+  customDays: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  customMonths: [
+    "Январь",
+    "Февраль",
+    "Март",
+    "Апрель",
+    "Май",
+    "Июнь",
+    "Июль",
+    "Август",
+    "Сентябрь",
+    "Октябрь",
+    "Ноябрь",
+    "Декабрь",
+  ],
+});
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,3 +1,4 @@
 import "%modules%/header/header";
 import "%modules%/footer/footer";
 import "%modules%/popular/popular";
+import "%modules%/offers/offers";
