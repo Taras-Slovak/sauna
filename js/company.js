d79bee5142801414b4583efb1b92e3d9b16c82c3
Taras Slovak <slovak.taras@gmail.com>
2022-10-30 18:09:32 +0200

    added code

--- js/company.js
+++ js/company.js
@@ -42,6 +42,18 @@ getMainSideBar.addEventListener("click", function (e) {
   \*************************************************************************/
 /***/ (function() {
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 function companyDropdown(dropdown, menu, items) {
   var getBtn = document.querySelector(dropdown),
       getMenu = document.querySelector(menu),
@@ -64,6 +76,59 @@ function companyDropdown(dropdown, menu, items) {
 
 companyDropdown("#dropdown-city", "#menu-city", ".company-contacts__dropdown-city");
 companyDropdown("#dropdown-street", "#menu-street", ".company-contacts__dropdown-street");
+
+function addOrRemove() {
+  var getMainField = document.querySelector(" .company-contacts__telephone-group");
+  var getAddButtons = document.querySelector(".company-contacts__telephone-add");
+
+  function addNewElement() {
+    var newTelephone = document.createElement("div");
+    newTelephone.classList.add("company-contacts__telephone");
+    newTelephone.innerHTML = "\n    <div class=\"company-contacts__field\">\n      <label for=\"telephone\">\u0422\u0435\u043B\u0435\u0444\u043E\u043D</label>\n      <input id=\"telephone\" name=\"telephone\" type=\"text\" placeholder=\"+38 067 123-23-23\">\n    </div>\n    <button class=\"company-contacts__telephone-delete\" type=\"button\">\n      <svg width=\"26\" height=\"26\" viewBox=\"0 0 26 26\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n        <circle cx=\"13\" cy=\"13\" r=\"12\" fill=\"#2F2F2F\"></circle>\n        <path fill-rule=\"evenodd\" clip-rule=\"evenodd\" d=\"M9.9505 8.88994C9.65761 8.59705 9.18274 8.59705 8.88984 8.88994C8.59695 9.18284 8.59695 9.65771 8.88984 9.9506L11.9392 13L8.88984 16.0494C8.59695 16.3423 8.59695 16.8172 8.88984 17.1101C9.18274 17.403 9.65761 17.403 9.9505 17.1101L12.9999 14.0607L16.0493 17.1101C16.3422 17.403 16.8171 17.403 17.11 17.1101C17.4029 16.8172 17.4029 16.3423 17.11 16.0494L14.0606 13L17.11 9.9506C17.4029 9.65771 17.4029 9.18284 17.11 8.88994C16.8171 8.59705 16.3422 8.59705 16.0493 8.88994L12.9999 11.9393L9.9505 8.88994Z\"></path>\n      </svg>\n    </button>\n    ";
+    getMainField.appendChild(newTelephone);
+  }
+
+  getAddButtons.addEventListener("click", function () {
+    addNewElement();
+
+    _toConsumableArray(getMainField.children).forEach(function (element) {
+      var btn = element.firstElementChild.nextElementSibling;
+      btn.addEventListener("click", function () {
+        if (btn.classList.contains("company-contacts__telephone-delete")) element.remove();
+      });
+    });
+  });
+}
+
+addOrRemove();
+
+function removeAddTime() {
+  var getAddBtn = document.querySelector(".company-contacts__time-add");
+  getAddBtn.addEventListener("click", function () {
+    createNewDate();
+    var getAllDelete = document.querySelectorAll(".company-contacts__date");
+    getAllDelete.forEach(function (item) {
+      var getDeleteBtn = item.firstElementChild.nextElementSibling.nextElementSibling;
+      getDeleteBtn.addEventListener("click", function () {
+        if (getDeleteBtn.classList.contains("company-contacts__time-delete")) item.remove();
+      });
+    });
+  });
+}
+
+removeAddTime();
+
+function companyDropdownTime() {}
+
+companyDropdownTime();
+
+function createNewDate() {
+  var getDatePicker = document.querySelector(".company-contacts__date-picker");
+  var newDate = document.createElement("div");
+  newDate.classList.add("company-contacts__date");
+  newDate.innerHTML = "\n\n  <div class=\"company-contacts__days\">\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u041F\u043D\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u0412\u0442\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u0421\u0440\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u0427\u0442\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u041F\u0442\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u0421\u0431\n    </button>\n    <button\n      class=\"company-contacts__day\"\n      type=\"button\"\n      onclick=\"this.classList.toggle('background-grey')\"\n    >\n      \u0412\u0441\n    </button>\n</div>\n<div class=\"company-contacts__times\">\n  <div class=\"company-contacts__field\">\n    <button\n      class=\"company-contacts__dropdown-button\"\n      id=\"dropdown-hour\"\n      type=\"button\"\n    >\n      <p class=\"company-contacts__dropdown-txt\">00:00</p>\n      <svg\n        width=\"20\"\n        height=\"20\"\n        viewBox=\"0 0 20 20\"\n        fill=\"none\"\n        xmlns=\"http://www.w3.org/2000/svg\"\n      >\n        <path\n          d=\"M6.175 7.1582L10 10.9749L13.825 7.1582L15 8.3332L10 13.3332L5 8.3332L6.175 7.1582Z\"\n          fill=\"#2F2F2F\"\n        />\n      </svg>\n      <div class=\"company-contacts__dropdown-menu\" id=\"menu-hour\">\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"00:00\">\n          00:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"01:00\">\n          01:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"02:00\">\n          02:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"03:00\">\n          03:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"04:00\">\n          04:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"05:00\">\n          05:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"06:00\">\n          06:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"07:00\">\n          07:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"08:00\">\n          08:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"09:00\">\n          09:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"10:00\">\n          10:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"11:00\">\n          11:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"12:00\">\n          12:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"13:00\">\n          13:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"14:00\">\n          14:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"15:00\">\n          15:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"16:00\">\n          16:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"17:00\">\n          17:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"18:00\">\n          18:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"19:00\">\n          19:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"20:00\">\n          20:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"21:00\">\n          21:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"22:00\">\n          22:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"23:00\">\n          23:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"24:00\">\n          24:00\n        </span>\n      </div>\n    </button>\n  </div>\n  <span class=\"company-contacts__line\"> </span>\n  <div class=\"company-contacts__field\">\n    <button\n      class=\"company-contacts__dropdown-button\"\n      id=\"dropdown-hour\"\n      type=\"button\"\n    >\n      <p class=\"company-contacts__dropdown-txt\">00:00</p>\n      <svg\n        width=\"20\"\n        height=\"20\"\n        viewBox=\"0 0 20 20\"\n        fill=\"none\"\n        xmlns=\"http://www.w3.org/2000/svg\"\n      >\n        <path\n          d=\"M6.175 7.1582L10 10.9749L13.825 7.1582L15 8.3332L10 13.3332L5 8.3332L6.175 7.1582Z\"\n          fill=\"#2F2F2F\"\n        />\n      </svg>\n      <div class=\"company-contacts__dropdown-menu\" id=\"menu-hour\">\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"00:00\">\n          00:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"01:00\">\n          01:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"02:00\">\n          02:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"03:00\">\n          03:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"04:00\">\n          04:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"05:00\">\n          05:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"06:00\">\n          06:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"07:00\">\n          07:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"08:00\">\n          08:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"09:00\">\n          09:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"10:00\">\n          10:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"11:00\">\n          11:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"12:00\">\n          12:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"13:00\">\n          13:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"14:00\">\n          14:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"15:00\">\n          15:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"16:00\">\n          16:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"17:00\">\n          17:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"18:00\">\n          18:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"19:00\">\n          19:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"20:00\">\n          20:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"21:00\">\n          21:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"22:00\">\n          22:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"23:00\">\n          23:00\n        </span>\n        <span class=\"company-contacts__dropdown-hour\" data-hour=\"24:00\">\n          24:00\n        </span>\n      </div>\n    </button>\n  </div>\n</div>\n<button class=\"company-contacts__time-delete\" type=\"button\">\n  <svg\n    width=\"26\"\n    height=\"26\"\n    viewBox=\"0 0 26 26\"\n    fill=\"none\"\n    xmlns=\"http://www.w3.org/2000/svg\"\n  >\n    <circle cx=\"13\" cy=\"13\" r=\"12\" fill=\"#2F2F2F\"></circle>\n    <path\n      fill-rule=\"evenodd\"\n      clip-rule=\"evenodd\"\n      d=\"M9.9505 8.88994C9.65761 8.59705 9.18274 8.59705 8.88984 8.88994C8.59695 9.18284 8.59695 9.65771 8.88984 9.9506L11.9392 13L8.88984 16.0494C8.59695 16.3423 8.59695 16.8172 8.88984 17.1101C9.18274 17.403 9.65761 17.403 9.9505 17.1101L12.9999 14.0607L16.0493 17.1101C16.3422 17.403 16.8171 17.403 17.11 17.1101C17.4029 16.8172 17.4029 16.3423 17.11 16.0494L14.0606 13L17.11 9.9506C17.4029 9.65771 17.4029 9.18284 17.11 8.88994C16.8171 8.59705 16.3422 8.59705 16.0493 8.88994L12.9999 11.9393L9.9505 8.88994Z\"\n    ></path>\n  </svg>\n</button>\n  \n  ";
+  getDatePicker.appendChild(newDate);
+}
 
 /***/ }),
 
