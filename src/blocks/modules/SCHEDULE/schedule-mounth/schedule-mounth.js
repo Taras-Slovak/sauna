8726469d57f53c1a7ade83c0d1378fe14aec69c0
Taras Slovak <slovak.taras@gmail.com>
2022-11-27 12:07:53 +0200

    added code

--- src/blocks/modules/SCHEDULE/schedule-mounth/schedule-mounth.js
+++ src/blocks/modules/SCHEDULE/schedule-mounth/schedule-mounth.js
@@ -1,6 +1,6 @@
 function toggleSchedule() {
-  const getLeftBtn = document.querySelector('.schedule-mounth__toggle-left');
-  const getRightBtn = document.querySelector('.schedule-mounth__toggle-right');
+  const getLeftBtn = document.querySelector('.schedule-month__toggle-left');
+  const getRightBtn = document.querySelector('.schedule-month__toggle-right');
 
   getLeftBtn.onclick = () => {
     if (!getLeftBtn.classList.contains('active-toggle')) {
@@ -17,3 +17,18 @@ function toggleSchedule() {
   };
 }
 toggleSchedule();
+
+function currentDaysOfMonth() {
+  const getCurrentDay = document.querySelector('.schedule-month__nav-text');
+  const getAllDays = document.querySelectorAll('.schedule-month__day');
+  getAllDays.forEach((day) => {
+    if (getCurrentDay.dataset.month === day.dataset.month) {
+      day.style.opacity = '1';
+    } else if (day.dataset.month == undefined || day.dataset.month == null) {
+      day.style.opacity = '1';
+    } else {
+      day.style.opacity = '0.5';
+    }
+  });
+}
+currentDaysOfMonth();
