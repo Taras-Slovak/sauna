a20eff7b46dcc480f5e80ed6d94c851153b00ccc
Taras Slovak <slovak.taras@gmail.com>
2022-12-02 17:22:13 +0200

    added code

--- src/blocks/modules/SCHEDULE/schedule-price/schedule-price.js
+++ src/blocks/modules/SCHEDULE/schedule-price/schedule-price.js
@@ -11,3 +11,18 @@ function getText() {
   });
 }
 getText();
+
+function currentDaysOfMonth() {
+  const getCurrentDay = document.querySelector('.schedule-price__nav-text');
+  const getAllDays = document.querySelectorAll('.schedule-price__day');
+  getAllDays.forEach((day) => {
+    if (getCurrentDay.dataset.month === day.dataset.month) {
+      day.style.opacity = '1';
+    } else if (day.dataset.month == undefined || day.dataset.month == null) {
+      day.style.opacity = '1';
+    } else {
+      day.style.opacity = '0.5';
+    }
+  });
+}
+currentDaysOfMonth();
