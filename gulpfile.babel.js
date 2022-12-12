55b7413d625428929c43e47bce710677394317bd
Taras Slovak <slovak.taras@gmail.com>
2022-12-12 19:32:41 +0200

    aded code

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,7 +11,7 @@ const requireDir = require('require-dir'),
     },
     styles: {
       // src: './src/styles/*.{scss,sass}',
-      src: './src/styles/account-from.scss',
+      src: './src/styles/account-paid.scss',
       dist: './dist/styles/',
       watch: ['./src/blocks/**/*.{scss,sass}', './src/styles/**/*.{scss,sass}'],
     },
